76a821e1ad5d816c4f11ff801f093f0c861220d2
Dalton Neely <daltonjn1@gmail.com>
2021-02-20 21:02:53 -0700

    feat: Add Links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 export const Home = () => {
     return (
         <>
             Home
+            <Link to='/movies'>Movies</Link>
+            <Link to='/series'>Series</Link>
         </>
     )
 }
