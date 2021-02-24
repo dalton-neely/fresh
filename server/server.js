0f18b8a42ec5434e400d7ed09a2f8f3b5f735d17
Dalton Neely <daltonjn1@gmail.com>
2021-02-23 18:09:16 -0700

    feat: Add Backend

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
+const fs = require('fs');
 
 const app = new Koa();
 const router = new Router();
 
+let jsonData = null;
+
+fs.readFile("./feed/data.json", (error, data) => {
+    if (error) {
+        console.log("Cannot read feed file: " + error.message)
+    } else {
+        jsonData = JSON.parse(data.toString())
+    }
+});
+
 router.get('/data-feed', (ctx, next) => {
-  // stubbed endpoint
+    ctx.response.status = 200;
+    ctx.body = jsonData
+    next();
 });
 
 app
-  .use(router.routes())
-  .use(router.allowedMethods());
+    .use(router.routes())
+    .use(router.allowedMethods());
 
 
 app.listen(3001)
 
-console.log('Server running at localhost:3001', )
+console.log('Server running at localhost:3001',)
